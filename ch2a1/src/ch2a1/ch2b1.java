fbde164fa7e05cfa94de3763e583c7bd5d8a517f
XuYangFang <xuyangfff@gmail.com>
2022-09-01 20:41:52 -0500

    ch2b1
    ch2b1 inclass

---
+++ ch2a1/src/ch2a1/ch2b1.java
@@ -0,0 +1,12 @@
+package ch2a1;
+
+public class ch2b1 {
+final int EXAMPEL = 4;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int EXAMPLE = 5;
+		System.out.println(EXAMPLE);
+		
+	}
+
+}
