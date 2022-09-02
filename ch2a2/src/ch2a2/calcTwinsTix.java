4f3a88d37783051a50cfbf984551fe74eb1aabd5
XuYangFang <xuyangfff@gmail.com>
2022-09-01 19:34:18 -0500

    ch2a2
    cha2a2 inclass

---
+++ ch2a2/src/ch2a2/calcTwinsTix.java
@@ -0,0 +1,26 @@
+package ch2a2;
+import javax.swing.JOptionPane;
+
+public class calcTwinsTix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String tickets;
+		
+		final int price = 75;
+		final int processing_fee = 20;
+		final double sales_tax = 0.07;
+		
+		tickets = JOptionPane.showInputDialog(null, "number of tickets?");
+		
+		final double tax = 75 * 0.07;
+		double cost = Double.parseDouble(tickets) * price + Double.parseDouble(tickets) * processing_fee + tax;
+	
+		
+		JOptionPane.showMessageDialog(null, "the total is " + cost);
+		
+				
+	}
+
+}
